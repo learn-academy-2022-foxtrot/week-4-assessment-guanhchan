757961738da296ba46a79f73be6609fea980e21f
guanhchan <guanh.chan@gmail.com>
2022-09-30 15:40:29 -0700

    week_4_assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -9,42 +9,128 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
+describe('greeter', () => {
+    it("return hi, fox", () => {
+        expect(greeter()).toEqual('hi, fox!')
+    })
+})
 
+const greeter = () => {
+    return 'hi, fox!'
+}
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided. 
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe('arrShuffle', () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+    it("removes the first item from the array and shuffles the remaining content.", () => {
+
+        expect(arrShuffle(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+        expect(arrShuffle(colors2)).toEqual(expect.arrayContaining(["indigo", "periwinkle", "ochre", "aquamarine", "saffron"]))
+
+    })
+})
+
+// ReferenceError: arrShuffle is not defined
 
 // b) Create the function that makes the test pass.
+
+// So I need to make a function that takes in an array and removes the first item and shuffles the rest
+// To access the first item I would use a method color1.shift()
+// Then take the color1 array and shuffle the items in the array, with .sort() and Math.random()
+// Then return the new array
+
+const arrShuffle = (arr) => {
+    arr.shift()
+    arr.sort(() => Math.random() - 0.5)
+    return arr
+}
+
+// arrShuffle
+// ✓ removes the first item from the array and shuffles the remaining content.
+
 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the net total of votes.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = {upVotes: 13, downVotes: 2}
-// Expected output: 11
-const votes2 = {upVotes: 2, downVotes: 33}
-// Expected output: -31
+describe('voteCount', () => {
+    const votes1 = { upVotes: 13, downVotes: 2 }
+    // Expected output: 11
+    const votes2 = { upVotes: 2, downVotes: 33 }
+    // Expected output: -31
+    it("takes in an object that contains up votes and down votes and returns the net total of votes.", () => {
+        expect(voteCount(votes1)).toEqual(11)
+        expect(voteCount(votes2)).toEqual(-31)
 
+    })
+})
+
+// ● voteCount › takes in an object that contains up votes and down votes and returns the net total of votes.
+
+// ReferenceError: voteCount is not defined
 
 // b) Create the function that makes the test pass.
+
+// Im going to write a function that takes in a obj
+// Then access the keys and values in the obj
+// Then take the upVotes and subtract from downVotes
+// Then return the final value
+
+const voteCount = (obj) => {
+    let votes = obj.upVotes - obj.downVotes
+    return votes
+}
+
+// voteCount
+// ✓ takes in an object that contains up votes and down votes and returns the net total of votes.
+
+// Test Suites: 1 passed, 1 total
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+describe('arrAddNoDupl', () => {
+    const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+    const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+    // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+    it("takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+        expect(arrAddNoDupl(dataArray1,dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+    })
+})
 
+// ● arrAddNoDupl › takes in two arrays as arguments and returns one array with no duplicate values.
+
+// ReferenceError: arrAddNoDupl is not defined
 
 // b) Create the function that makes the test pass.
+
+// I need to write a func that take in to arg and return one array with no duplicates values
+// First I will need an empty arr to push in all the items in the array.
+// Then I can combine the arrays together
+// Then I will loop through the array and push each new item into the new arr
+// I will use a conditional to check it its in the arr and it not it will push into the new array
+// Then return the new array.
+
+const arrAddNoDupl = (arr1,arr2) => {
+    newArr = []
+    let arrConcat = arr1.concat(arr2)
+    for(let i = 0; i <arrConcat.length ;i++ ){
+        if(!newArr.includes(arrConcat[i])){
+            newArr.push(arrConcat[i])
+        }
+    }
+    return newArr
+}
+
+// Great this worked as expected, glad to know I still got it.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,7 +10,21 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def num_check_eve_or_odd num
+    if(num.even?)
+        p "#{num} is even"
+    elsif(num.odd?)
+        p "#{num} is odd"
+    end
+end
 
+num_check_eve_or_odd num1
+
+num_check_eve_or_odd num2
+
+num_check_eve_or_odd num3
+
+# Great it worked as expected
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
@@ -22,7 +36,17 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def vowel_remover string
+    string.delete "aeiouAEIOU"
+end
 
+p vowel_remover beatles_album1
+
+p vowel_remover beatles_album2
+
+p vowel_remover beatles_album3
+
+# Great!, The link was helpful thank you. =)
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
@@ -32,3 +56,19 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def palindrome_checker string
+    if string == string.reverse
+        p "#{string} is a palindrome"
+    else
+        p "#{string} is not a palindrome"
+    end
+end
+
+palindrome_checker(palindrome_tester1)
+
+palindrome_checker palindrome_tester2
+
+palindrome_checker (palindrome_tester3)
+
+# Yay done with ruby challenges.
